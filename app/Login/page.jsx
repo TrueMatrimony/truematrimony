f27751a7e19222f00dc71e9b9640ca7a92594291
TrueMatrimony <sabatcps@gmail.com>
2024-12-08 18:13:03 +0530

    New Changes

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -4,13 +4,13 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Login() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  // Perform the redirect if the user is already logged in
-  if (session) {
-    // Redirect to the dashboard if the session exists
-    redirect("/dashboard");
-  }
+  // // Perform the redirect if the user is already logged in
+  // if (session) {
+  //   // Redirect to the dashboard if the session exists
+  //   redirect("/dashboard");
+  // }
 
   // If no session, render the LoginForm
   return <LoginForm />;
