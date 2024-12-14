2f493852c6299fd7cef81a654909f7bd36fbdb27
TrueMatrimony <sabatcps@gmail.com>
2024-12-14 15:48:30 +0530

    check

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -23,7 +23,9 @@ export const authOptions = {
             return null;
           }
           return user;
-        } catch (error) {}
+        } catch (error) {
+          
+        }
       },
     }),
   ],
